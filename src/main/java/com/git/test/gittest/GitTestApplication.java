86e2239194f2007ffa55027a48f5610a62dbc226
jordan.ross <jord_ross@hotmail.com>
2018-02-27 05:33:23 -0700

    Added a cool string method

--- src/main/java/com/git/test/gittest/GitTestApplication.java
+++ src/main/java/com/git/test/gittest/GitTestApplication.java
@@ -6,7 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GitTestApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GitTestApplication.class, args);
-	}
+    public static void main(String[] args) {
+        System.out.println(getCoolString());
+        SpringApplication.run(GitTestApplication.class, args);
+    }
+
+    private static String getCoolString() {
+        return "This is cool";
+    }
 }
