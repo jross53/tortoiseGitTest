b4ce441f47b9968559e16a95e03b74d5a7f4af46
jordan.ross <jord_ross@hotmail.com>
2018-02-27 05:50:22 -0700

    Changed to great string

--- src/main/java/com/git/test/gittest/GitTestApplication.java
+++ src/main/java/com/git/test/gittest/GitTestApplication.java
@@ -8,7 +8,7 @@ public class GitTestApplication {
 
     public static void main(String[] args) {
         System.out.println(getVeryCoolString());
-        System.out.println(getThisAmazingString());
+        System.out.println(getThisGreatString());
         SpringApplication.run(GitTestApplication.class, args);
     }
 
@@ -16,7 +16,7 @@ public class GitTestApplication {
         return "This is very cool";
     }
 
-    private static String getThisAmazingString() {
-        return "This is amazing";
+    private static String getThisGreatString() {
+        return "This is great";
     }
 }
